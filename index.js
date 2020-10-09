0a1fc5c90d9a829cc6f916bed132dd798ab28b6b
rishav <rishav.21m@gmail.com>
2020-10-09 23:25:33 +0530

    made it collaipsible

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-import { select, json, tree, hierarchy, linkHorizontal, zoom, event } from "d3";
+import {
+  select,
+  json,
+  tree,
+  hierarchy,
+  create,
+  linkHorizontal,
+  zoom,
+  event,
+} from "d3";
 
-const svg = select("svg");
 const toggle = select("p");
 const width = document.body.clientWidth;
 const height = document.body.clientHeight;
+
+const dx = 10;
+const dy = width / 6;
+const diagonal = linkHorizontal()
+  .x((d) => d.y)
+  .y((d) => d.x);
 
 const margin = { top: 0, right: 50, bottom: 0, left: 75 };
 const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
 
-const treeLayout = tree().size([innerHeight, innerWidth]);
+const svg = select("svg");
 
-const zoomG = svg.attr("width", width).attr("height", height).append("g");
+const treeLayout = tree().size([innerHeight, innerWidth - 115]);
 
-const g = zoomG
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+const zoomG = svg.attr("width", width).attr("height", height);
+svg.call(zoom().scaleExtent([0.5, 5]).on("zoom", zoom));
+
+const g = zoomG.attr("transform", `translate(${margin.left},${margin.top})`);
 
 svg.call(
   zoom().on("zoom", ({ transform }) => {
@@ -25,38 +40,147 @@ svg.call(
 
 const renderTree = (data) => {
   const root = hierarchy(data);
-  const links = treeLayout(root).links();
+  root.x0 = dy / 2;
+  root.y0 = 0;
+  root.descendants().forEach((d, i) => {
+    d.id = i;
+    d._children = d.children;
+    if (d.depth && d.data.name.length !== 7) d.children = null;
+  });
 
-  tree(root);
-  const linkPathGenerator = linkHorizontal()
-    .x((d) => d.y)
-    .y((d) => d.x);
+  const gLink = svg
+    .append("g")
+    .attr("fill", "none")
+    .attr("stroke", "#555")
+    .attr("stroke-opacity", 0.4)
+    .attr("stroke-width", 1.5);
 
-  g.selectAll("path")
-    .data(links)
-    .enter()
-    .append("path")
-    .attr("d", linkPathGenerator);
+  const gNode = svg
+    .append("g")
+    .attr("cursor", "pointer")
+    .attr("pointer-events", "all");
 
-  g.selectAll("text")
-    .data(root.descendants().reverse())
-    .enter()
-    .append("text")
-    .attr("x", (d) => d.y)
-    .attr("y", (d) => d.x)
-    .attr("dy", "0.32em")
-    .attr("text-anchor", (d) => (d.children ? "middle" : "start"))
-    .attr("font-size", (d) => 3.25 - d.depth + "em")
-    .text((d) => d.data.data.id);
+  function update(source) {
+    const duration = d3.event && d3.event.altKey ? 2500 : 250;
+    const nodes = root.descendants().reverse();
+    const links = root.links();
+
+    // Compute the new tree layout.
+    treeLayout(root);
+
+    let left = root;
+    let right = root;
+    root.eachBefore((node) => {
+      if (node.x < left.x) left = node;
+      if (node.x > right.x) right = node;
+    });
+
+    const height = right.x - left.x + margin.top + margin.bottom;
+
+    const transition = svg
+      .transition()
+      .duration(duration)
+      .attr("viewBox", [-margin.left, left.x - margin.top, width, height])
+      .tween(
+        "resize",
+        window.ResizeObserver ? null : () => () => svg.dispatch("toggle")
+      );
+
+    // Update the nodes…
+    const node = gNode.selectAll("g").data(nodes, (d) => d.id);
+
+    // Enter any new nodes at the parent's previous position.
+    const nodeEnter = node
+      .enter()
+      .append("g")
+      .attr("transform", (d) => `translate(${source.y0},${source.x0})`)
+      .attr("fill-opacity", 0)
+      .attr("stroke-opacity", 0)
+      .on("click", (event, d) => {
+        d.children = d.children ? null : d._children;
+        update(d);
+      });
+
+    nodeEnter
+      .append("circle")
+      .attr("r", 2.5)
+      .attr("fill", (d) => (d._children ? "#555" : "#999"))
+      .attr("stroke-width", 10);
+
+    nodeEnter
+      .append("text")
+      .attr("dy", "0.31em")
+      .attr("x", (d) => (d._children ? -6 : 6))
+      .attr("text-anchor", (d) => (d._children ? "end" : "start"))
+      .text((d) => d.data.name)
+      .clone(true)
+      .lower()
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-width", 3)
+      .attr("stroke", "white");
+
+    // Transition nodes to their new position.
+    const nodeUpdate = node
+      .merge(nodeEnter)
+      .transition(transition)
+      .attr("transform", (d) => `translate(${d.y},${d.x})`)
+      .attr("fill-opacity", 1)
+      .attr("stroke-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    const nodeExit = node
+      .exit()
+      .transition(transition)
+      .remove()
+      .attr("transform", (d) => `translate(${source.y},${source.x})`)
+      .attr("fill-opacity", 0)
+      .attr("stroke-opacity", 0);
+
+    // Update the links…
+    const link = gLink.selectAll("path").data(links, (d) => d.target.id);
+
+    // Enter any new links at the parent's previous position.
+    const linkEnter = link
+      .enter()
+      .append("path")
+      .attr("d", (d) => {
+        const o = { x: source.x0, y: source.y0 };
+        return diagonal({ source: o, target: o });
+      });
+
+    // Transition links to their new position.
+    link.merge(linkEnter).transition(transition).attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link
+      .exit()
+      .transition(transition)
+      .remove()
+      .attr("d", (d) => {
+        const o = { x: source.x, y: source.y };
+        return diagonal({ source: o, target: o });
+      });
+
+    // Stash the old positions for transition.
+    root.eachBefore((d) => {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
+  }
+  update(root);
+  return svg.node();
 };
 
-json("data.json").then((data) => {
+json("flare-2.json").then((data) => {
   renderTree(data);
 });
 
 //toogle data
 toggle.on("click", function () {
-  json("data2.json").then((data) => {
+  json("flare-2.json").then((data) => {
+    svg.selectAll("path").remove();
+    svg.selectAll("circle").remove(); // and so on
+    svg.selectAll("text").remove(); // and so on
     renderTree(data);
   });
 });
